8aa005ff8aeacf1e3233fb95fb8cf1ef3d9c2876
Niedzwiedz1 <niewadzilukasz@gmail.com>
2023-11-29 17:50:54 +0000

    Render descriptions, images, source code links

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ Bellow you can find some of the apps I have build in Python.
             """
 st.write(content1)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
@@ -32,10 +32,14 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
 
